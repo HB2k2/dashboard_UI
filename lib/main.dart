a81894efc400f398e9dd6408da46d59e2e48bf5e
HB2k2 <hiteshbharadwaj526@yahoo.in>
2024-03-06 22:09:18 +0530

    deleted commented code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// import 'package:admin/controllers/MenuAppController.dart';
-// import 'package:admin/screens/main/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
